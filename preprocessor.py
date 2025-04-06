006909f00567e9ee292ed5412f85ba797cb93642
Ilia Gilmijarow <gilmijar@gmail.com>
2025-04-07 01:27:15 +0200

    Last missing function with unittests and an integration test to see if they work together

--- preprocessor.py
+++ preprocessor.py
@@ -1,17 +1,29 @@
-from collections.abc import Mapping
 from typing import Any, Callable
 
-def add_namespace(data:dict, ns: str|None)->dict:
+
+def get_namespace(whence: str) -> str | None:
+    """Extracts the namespace from a colon-delimited string."""
+    return whence.rpartition(':')[0]
+
+
+def add_namespace(data: dict, ns: str | None) -> dict:
+    """Applies a namespace prefix to keys that lack one."""
     if not ns:
         return dict(data)
     else:
-        return {(k if ':' in k else f"{ns}:{k}"): v for k, v in data.items()}
+        return {
+            (k if ':' in k else f"{ns}:{k}"): v
+            for k, v
+            in data.items()
+        }
 
 
-def traverse(data:dict|Any, fn=Callable)->dict:
-    if not isinstance(data, Mapping):
+def namespace_traverse(data: dict | Any, fn: Callable, ns: str | None = None) -> dict:
+    """Recursively apply a function to a nested dictionary structure, maintaining namespace context."""
+    if not isinstance(data, dict):
         return data
     new_data = {}
-    for k, v in fn(data).items():
-        new_data[k] = traverse(v, fn)
+    for k, v in fn(data, ns).items():
+        _ns = get_namespace(k)
+        new_data[k] = namespace_traverse(v, fn, _ns)
     return new_data
--- preprocessor_tests.py
+++ preprocessor_tests.py
@@ -1,6 +1,6 @@
 import unittest
-from preprocessor import add_namespace
-from preprocessor import traverse
+from preprocessor import add_namespace, namespace_traverse, get_namespace
+
 
 class TestsAddNamespace(unittest.TestCase):
     def test_when_no_namespace_given_do_nothing(self):
@@ -41,22 +41,68 @@ class TestsAddNamespace(unittest.TestCase):
 class TestsTraverseDict(unittest.TestCase):
     def test_when_no_nesting_return_value(self):
         provided = {'a': 'leaf_value'}
-        received = traverse(provided, lambda d: d)
+        received = namespace_traverse(provided, fn=lambda d, _: d)
         self.assertEqual(provided['a'], received['a'])
 
     def test_when_values_are_nested_return_traversed_values(self):
         provided = {'a': {'b':'nested_leaf'}, 'c': 'leaf_value'}
-        received = traverse(provided, lambda d: d)
+        received = namespace_traverse(provided, fn=lambda d, _: d)
         expected = {'a': {'b':'nested_leaf'}, 'c': 'leaf_value'}
         self.assertEqual(expected['a'], received['a'])
 
     def test_when_fn_provided_return_after_application(self):
-        apply_me = lambda d: {k.upper(): v for k, v in d.items()}
+        apply_me = lambda d, _: {k.upper(): v for k, v in d.items()}
         provided = {'a': {'b': 'nested_leaf'}, 'c': 'leaf_value'}
-        received = traverse(provided, apply_me)
+        received = namespace_traverse(provided, fn=apply_me)
         expected = {'A': {'B': 'nested_leaf'}, 'C': 'leaf_value'}
         self.assertEqual(expected, received, 'Should return data with function applied')
 
+
+class TestsExtractNamespace(unittest.TestCase):
+    def test_when_namespace_not_given_return_empty_str(self):
+        received = get_namespace('dolor')
+        self.assertEqual('', received)
+        received = get_namespace(':dolor')
+        self.assertEqual('', received)
+
+    def test_when_namespace_is_given_return_namespace(self):
+        received = get_namespace('amet:dolor')
+        self.assertEqual('amet', received)
+
+
+class TestIntegrations(unittest.TestCase):
+    def setUp(self):
+        self.provided = {
+            'test_ns:top_node_1': {
+                'sub_1_node_1': {
+                    'sub_2_node_1': 'Lorem',
+                    'fixed_ns_l2:sub_2_node_2': 'Ipsum'
+                },
+                'fixed_ns_l1:sub_1_node_2': {
+                    'sub_2_node_3': 'Dolor',
+                    'sub_2_node_4': 'Sit Amet'
+                },
+                'sub_1_node_3': 'Consectetur'
+            }
+        }
+
+        self.expected = {
+            'test_ns:top_node_1': {
+                'test_ns:sub_1_node_1': {
+                    'test_ns:sub_2_node_1': 'Lorem',
+                    'fixed_ns_l2:sub_2_node_2': 'Ipsum'
+                },
+                'fixed_ns_l1:sub_1_node_2': {
+                    'fixed_ns_l1:sub_2_node_3': 'Dolor',
+                    'fixed_ns_l1:sub_2_node_4': 'Sit Amet'
+                },
+                'test_ns:sub_1_node_3': 'Consectetur'
+            }
+        }
+
+    def test_happy_path(self):
+        received = namespace_traverse(self.provided, fn=add_namespace)
+        self.assertDictEqual(received, self.expected)
 
 if __name__ == '__main__':
     unittest.main()
